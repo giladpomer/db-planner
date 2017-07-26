c1eac7a54653e1d3afc4e1d74057e48ec41038af
Gilad Shapira <giladshapira2@gmail.com>
2017-07-26 16:58:54 +0200

    E2E tests using protractor
    Tables section

---
+++ test/protractor.conf.js
@@ -0,0 +1,20 @@
+﻿exports.config = {
+    framework: 'jasmine',
+    baseUrl: 'http://localhost:3000/',
+    specs: [
+        'e2e/*.spec.js'
+    ],
+
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            // disable "chrome is being controlled by automated software"
+            'args': ['disable-infobars=true'],
+
+            // disable Password manager popup
+            'prefs': {
+                'credentials_enable_service': false
+            }
+        }
+    }
+}
