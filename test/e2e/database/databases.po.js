24f37b84c4389f535784a74a0a98dc9208513b3b
Gilad Shapira <giladshapira2@gmail.com>
2017-08-03 01:11:45 +0200

    Tests: Delete database

---
+++ test/e2e/database/databases.po.js
@@ -0,0 +1,48 @@
+﻿var Databases = function () {
+    var DatabaseItem = function (element) {
+        this.getName = function () {
+            return element
+                .element(by.binding('database.name'))
+                .getText();
+        };
+        this.delete = function () {
+            element
+                .element(by.css('.delete-database-button'))
+                .click();
+        };
+    };
+
+    var toggleDatabases = element(by.id('toggleDatabases'));
+    var closeDatabases = element(by.id('closeDatabases'));
+    var databaseNameEditor = element(by.model('databaseNameEditor'));
+    var addDatabaseButton = element(by.id('addDatabaseButton'));
+    var databasesList = element.all(by.repeater('database in databases'));
+
+    this.showList = function () {
+        toggleDatabases.click();
+    };
+    this.hideList = function () {
+        closeDatabases.click();
+    };
+
+    this.setEditorText = function (databaseName) {
+        databaseNameEditor.sendKeys(databaseName);
+    };
+
+    this.clickAdd = function () {
+        addDatabaseButton.click();
+    };
+    this.add = function (databaseName) {
+        this.setEditorText(databaseName);
+        this.clickAdd();
+    };
+
+    this.count = function () {
+        return databasesList.count();
+    };
+
+    this.getLast = function () {
+        return new DatabaseItem(databasesList.last());
+    };
+};
+module.exports = Databases;
---
+++ test/e2e/database/databases.spec.js
@@ -0,0 +1,50 @@
+﻿var Databases = require('./databases.po.js');
+var Tables = require('../tables/tables.po.js');
+var ConfirmDialog = require('../elements/confirmDialog.po.js');
+
+describe('Databases', function () {
+    var databases = new Databases();
+
+    beforeEach(function () {
+        browser.get(browser.baseUrl);
+    });
+
+    describe('Delete', function () {
+        beforeEach(function () {
+            var databaseName = 'DatabaseName';
+            databases.showList();
+            expect(databases.count()).toEqual(1);
+            databases.add(databaseName);
+            databases.showList();
+            expect(databases.count()).toEqual(2);
+        });
+
+        it('empty database', function () {
+            databases.getLast().delete();
+            expect(databases.count()).toEqual(1);
+        });
+
+        describe('Database with Tables', function () {
+            var tables = new Tables();
+            var confirmDialog = new ConfirmDialog();
+
+            beforeEach(function () {
+                databases.hideList();
+                tables.add('TableName');
+                databases.showList();
+                databases.getLast().delete();
+            });
+
+            it('and confirm', function () {
+                confirmDialog.confirm();
+                databases.showList();
+                expect(databases.count()).toEqual(1);
+            });
+
+            it('and cancel', function () {
+                confirmDialog.cancel();
+                expect(databases.count()).toEqual(2);
+            });
+        });
+    });
+});
